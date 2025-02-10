b354fc7995ebd9177ce6cdeecb13246e1ec838a9
Krix <sudhanshukaul504@gmail.com>
2025-02-10 13:21:45 +0530

    Final Touches #1 (There Could Be More)

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,12 +7,15 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 def validate_user_id(value):
-    if not re.match(r'^[a-z._]+$', value):  
+    if not re.match(r'^[a-z0-9._]+$', value):  
         raise ValidationError("User ID can only contain lowercase letters, dots (.), and underscores (_).")
 
 def validate_password(value):
-    if not re.match(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', value):
-        raise ValidationError("Password must be at least 8 characters long and contain both letters and numbers.")
+    if not re.match(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$', value):
+        raise ValidationError(
+            "Password must be at least 8 characters long and include one uppercase letter, one lowercase letter, one number, and one special character."
+        )
+
 
 # Create your models here. 
 class User(models.Model):
@@ -21,13 +24,13 @@ class User(models.Model):
     password = models.CharField(max_length=80 ,  validators=[validate_password] , null=False)
     email = models.EmailField(unique=True)
     bio = models.CharField(max_length=100 , blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
     gender = models.CharField(
         max_length=1, 
         choices=[('M', 'Male'), ('F', 'Female'), ('P', 'Prefer not to say')],
         default='P' 
     )
-    profile_pic = models.ImageField(upload_to="profile_pics/" , null=True , default="profile_pics/profile.png")
+    profile_pic = models.ImageField(upload_to="profile_pics/" , null=True , blank=True ,  default="profile_pics/profile.png")
 
     def __str__(self):
         return self.username 
@@ -37,14 +40,14 @@ class Post(models.Model):
     post_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User , on_delete= models.CASCADE , related_name="posts")
     caption = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
 
     def __str__(self):
         return f"Post by {self.user.username}"
 
 class PostImage(models.Model):
     post = models.ForeignKey(Post , on_delete= models.CASCADE , related_name="post_images")
-    image = models.ImageField(upload_to="images/Post_pics")
+    image = models.ImageField(upload_to="images/Post_pics", default="")
 
     def __str__(self):
         return f"Image for Post {self.post.post_id}"
@@ -56,7 +59,7 @@ class Hashtags(models.Model):
     content_type = models.ForeignKey(ContentType , on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type","object_id") 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
 
     def __str__(self):
         return self.tag  
@@ -80,7 +83,7 @@ class Comment(models.Model):
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type','object_id') 
     commented_text = models.CharField(max_length=200 , blank=False )
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
 
     parent = models.ForeignKey('self' , blank=True , null=True ,on_delete=models.CASCADE)
 
@@ -93,7 +96,7 @@ class Comment(models.Model):
 class SavedPost(models.Model):
     post = models.ForeignKey(Post , on_delete= models.CASCADE , related_name="saved_posts") 
     user = models.ForeignKey(User , on_delete= models.CASCADE , related_name="saved_posts")
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
 
     class Meta:
         unique_together = ('post', 'user')  # duplication preventer
@@ -107,6 +110,7 @@ class Followers(models.Model):
     followed_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        verbose_name_plural = "Followers"
         unique_together = ('follower', 'following')  # duplication preventer
 
     def __str__(self):
@@ -116,30 +120,26 @@ class Story(models.Model):
     story_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="stories")
     image = models.ImageField(upload_to="images/story_pics/")
-    created_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField(default=timezone.now() + timedelta(hours=24))
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
+    expires_at = models.DateTimeField(default=lambda: timezone.now() + timedelta(hours=24))
 
     def __str__(self):
         return f"Story by {self.user.username}"
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notifications")
-
     message = models.CharField(max_length=255)
-
     is_read = models.BooleanField(default=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    notification_type = models.CharField(max_length=50, choices=(
-        ('INFO', 'info'),
-        ('WARNING', 'Warning'),
-        ('ERROR', 'Error'),
-        ('SUCCESS', 'Success')
-    ), default='INFO')
+    created_at = models.DateTimeField(auto_now_add=True , db_index=True)
+    notification_type = models.CharField(max_length=50, choices=[
+    ('INFO', 'info'),
+    ('WARNING', 'Warning'),
+    ('ERROR', 'Error'),
+    ('SUCCESS', 'Success'),
+    ], default='INFO')
 
     def __str__(self):
-        return "Notfication for {self.user.username}: {self.message}"
+        return f"Notfication for {self.user.username}: {self.message}"
     
     def mark_as_read(self):
         """Mark the notification as read"""
@@ -154,26 +154,36 @@ class Notification(models.Model):
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
-    subject = models.CharField(max_length=255)
-    content = models.TextField()
+    subject = models.CharField(max_length=299)
+    content = models.TextField(blank=True,null=True)
     sent_at = models.DateTimeField(auto_now_add=True)
+    read_at = models.DateTimeField(null=True, blank=True)
     is_read = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f"Message from {self.sender.username} to {self.recipient.username} - {self.subject}"
-
+    edited_at = models.DateTimeField(null=True, blank=True)
+    is_edited = models.BooleanField(default=False)
+    deleted_for_user = models.ManyToManyField(User,blank=True,related_name="deleted_message")
+    is_draft = models.BooleanField(default=False)
+    scheduled_at = models.DateTimeField(null=True, blank=True)
+    replied_to = models.ForeignKey('self',null=True, blank=True, on_delete=models.DO_NOTHING , related_name="replies")
+    forward_from = models.ForeignKey('self',null=True, blank=True, on_delete=models.DO_NOTHING , related_name="forwards")
+    
     def mark_as_read(self):
-        """Mark the message as read"""
         self.is_read = True
+        self.read_at = timezone.now()
         self.save()
 
-    def mark_as_unread(self):
-        """Mark the message as unread"""
-        self.is_read = False
+    def edit_message(self,new_content):
+        self.content = new_content
+        self.edited_at = timezone.now()
+        self.is_edited = True
         self.save()
+        return self.content
+    def delete_for_user(self ,user):
+        self.deleted_for_user.add(user)
 
-    @property
-    def time_since_sent(self):
-        """Property to check the time passed since the message was sent"""
-        from django.utils import timezone
-        return timezone.now() - self.sent_at
+    def delete_message(self, user, for_everyone=False):
+        if for_everyone:
+            self.content = "[This message was deleted]"
+            self.save()
+        else:
+            self.deleted_for_user.add(user)
